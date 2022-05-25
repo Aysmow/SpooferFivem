c0616e3b61a8f113c2747bfb32ef8b2a7eeb475c
Aysmow <74377244+Aysmow@users.noreply.github.com>
2022-05-25 03:05:10 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,27 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+
+ ‎AYSMOW S'P00FER
 
 
 
-namespace AysmowAuth
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
+
+⭐ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴꜱ ⭐
+
+You got banned in Fivem?
+Then welcome to our private §poofer.
+This §poofer wont change your hwid everytime.
+and a lot of other stuff
+The method works completely different then
+all others out there. So it cant mess up
 
 
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
-        }
-    }
-}
+⭐ ꜰᴇᴀᴛᴜʀᴇꜱ ⭐ 
+
+- WORKING FIVEM BAN 14DAYS/ 255DAYS / ALL SERVERS
+- Wont change HWIDs, works completely different
+- No permanent system changes
+- Can't mess up your windows
+- Very unique spoof method
+- Remove auto shadow-bans
+- Comes with a cleaner
+- Remove HWID bans
+- Remove auto bans
+
+
+⭐ ᴘᴀʏᴍᴇɴᴛ ᴍᴇᴛʜᴏᴅꜱ ⭐ 
+Lifetime - 60$
+3 Months - $45
+1 Month- $10
